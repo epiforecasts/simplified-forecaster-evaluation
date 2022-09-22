e3e89cb5932a1a5b62e5ae0530fb55b865544431
Sam Abbott <contact@samabbott.co.uk>
2022-09-22 15:56:43 +0000

    add anomalies to metadata

--- data-raw/get-hub-metadata.R
+++ data-raw/get-hub-metadata.R
@@ -30,10 +30,44 @@ hub_forecasts <- hub_forecasts[grepl("inc case", target)]
 hub_forecasts <- hub_forecasts |>
   DT(!model %in% "EuroCOVIDhub-ensemble")
   
-# Streamline to metadata
-metadata <- hub_forecasts |>
+# Load anomalies
+anomalies <- fread(here("data", "anomalies.csv")) |>
+  DT(, location_name := NULL) |>
+  DT(, anomaly := TRUE)
+
+# Tag anomalies from evaluation set for truth data
+# Based on: https://github.com/covid19-forecast-hub-europe/covid19-forecast-hub-europe/blob/39823425e9ea5d66c3dc0e7a55fa7ba5433d7df2/code/evaluation/load_and_score_models.r#L29 # nolint
+metadata <- anomalies |>
+  copy() |>
+  DT(hub_forecasts, on = c("location", "target_end_date")) |>
+  DT(, previous_end_date := forecast_date - 2)
+
+metadata <- anomalies |>
+  DT(metadata,
+     on = c("target_end_date" = "previous_end_date", "location")
+  ) |>
+  DT(, c("target_end_date", "anomaly") := NULL) |>
+  setnames(
+    c("i.target_end_date", "i.anomaly"), c("target_end_date", "anomaly")
+  ) |>
+  DT(is.na(anomaly), anomaly := FALSE) |>
+  DT(, anomaly := as.logical(anomaly))
+
+# Streamline to unqiue forecast metadata
+# + count anomalies
+metadata <- metadata |>
+  DT(
+    target %in% c("1 wk ahead inc case", "2 wk ahead inc case",
+                  "3 wk ahead inc case", "4 wk ahead inc case")
+  ) |>
+  DT(, .(model, location, forecast_date, anomaly, target)) |>
+  unique() |>
+  DT(,
+   `:=`(anomaly = any(anomaly), n_anomaly = sum(anomaly)),
+   by = c("location", "forecast_date", "model")
+  ) |>
   DT(target %in% "1 wk ahead inc case") |>
-  DT(, .(model, location, forecast_date, target_end_date)) |>
+  DT(, .(model, location, forecast_date, anomaly, n_anomaly)) |>
   unique()
 
 # Save metadata
--- data-raw/update.sh
+++ data-raw/update.sh
@@ -4,9 +4,6 @@
 # Download hub forecasts of interest
 Rscript data-raw/get-hub-forecasts.R
 
-# Make hub metadata for study period
-Rscript data-raw/get-hub-metadata.R
-
 # Download truth data
 Rscript data-raw/get-truth.R
 
@@ -15,6 +12,9 @@ Rscript data-raw/get-population.R
 
 # Get anomalies data
 Rscript data-raw/get-anomalies.R
+
+# Make hub metadata for study period
+Rscript data-raw/get-hub-metadata.R
 
 # Calculate per forecast and quantile scores
 Rscript data-raw/score-forecasts.R
