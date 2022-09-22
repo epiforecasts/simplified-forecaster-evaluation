4faac722e3b288706c5fcadb45fab8f89b8dd8ae
Sam Abbott <contact@samabbott.co.uk>
2022-09-22 13:21:26 +0000

    add removal of forecasts made when data is anomalous

--- data-raw/score-forecasts.R
+++ data-raw/score-forecasts.R
@@ -21,14 +21,18 @@ forecasts_with_truth <- forecasts |>
   rescale_to_incidence_rate(population, scale = 1e4)
 
 # Load anomalies data
-anomalies <- fread(here("data", "anomalies.csv"))
+anomalies <- fread(here("data", "anomalies.csv")) |>
+  DT(, location_name := NULL)
 
-# Filter out anomalies from evaluation set
-clean_forecasts_truth <- forecasts_with_truth |>
-  DT(!anomalies, on = c("location", "target_end_date"))
+# Filter out anomalies from evaluation set for truth data
+clean_forecasts_w_truth <- forecasts_with_truth |>
+  DT(!anomalies, on = c("location", "target_end_date")) |>
+  DT(, previous_end_date := forecast_date - 2) |>
+  DT(!anomalies, on = c("previous_end_date" = "target_end_date", "location")) |>
+  DT(, previous_end_date := NULL)
 
 # Score forecasts
-scores <- score(clean_forecasts_truth)
+scores <- score(clean_forecasts_w_truth)
 
 # Save forecasts
 fwrite(scores, here("data", "scores.csv"))
